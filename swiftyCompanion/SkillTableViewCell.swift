574080b85acaea8beb5e951e2651a0a705f325e4
Martin SIREAU <martinsireau@Martins-MacBook-Pro.local>
2017-10-20 15:51:11 +0200

    first tableView ok

---
+++ swiftyCompanion/SkillTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SkillTableViewCell.swift
+//  swiftyCompanion
+//
+//  Created by Martin SIREAU on 20/10/2017.
+//  Copyright © 2017 Martin SIREAU. All rights reserved.
+//
+
+import UIKit
+
+class SkillTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- swiftyCompanion/VC/UserDatasViewController.swift
+++ swiftyCompanion/VC/UserDatasViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 import SwiftyJSON
 
-class UserDatasViewController: UIViewController {
+class UserDatasViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var userPic: UIImageView!
     @IBOutlet weak var myProgressView: UIProgressView!
+    
+    @IBOutlet weak var theScrollView: UIScrollView!
+    @IBOutlet weak var skillTableView: UITableView!
+    
+    let screenHeight = UIScreen.main.bounds.height
+    let scrollViewContentHeight = 1200 as CGFloat
     
     var myJson = JSON()
     
@@ -29,6 +35,30 @@ class UserDatasViewController: UIViewController {
         myProgressView.transform = myProgressView.transform.scaledBy(x: 1, y: 4)
         myProgressView.layer.cornerRadius = 5
         myProgressView.clipsToBounds = true
+        
+        theScrollView.contentSize = CGSize(width: self.view.frame.width, height: scrollViewContentHeight)
+//        scrollView.delegate = self
+//        tableView.delegate = self
+        theScrollView.bounces = false
+        skillTableView.bounces = false
+        skillTableView.isScrollEnabled = true
+
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = skillTableView.dequeueReusableCell(withIdentifier: "myCell")
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+
+        
+        cell.textLabel?.text = "lol\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
