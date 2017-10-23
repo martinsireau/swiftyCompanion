b231ef97141e692bdfc1c45d4f06972c766781b4
Martin SIREAU <martinsireau@Martins-MacBook-Pro.local>
2017-10-23 23:23:24 +0200

    ui ok

--- swiftyCompanion/VC/ViewController.swift
+++ swiftyCompanion/VC/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController, API42Delegate {
     
     var API42 : API42Controller?
     
+    
+    @IBOutlet weak var fetchDataLbl: UILabel!
+    @IBOutlet weak var mySpinWheel: UIActivityIndicatorView!
     @IBOutlet weak var myTextField: UITextField!
     
     func getUserInfo(myJson: JSON) {
@@ -21,6 +24,8 @@ class ViewController: UIViewController, API42Delegate {
             let alert = UIAlertController(title: "Info", message: "No Matching profiles with this Login", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
+            fetchDataLbl.isHidden = true
+            mySpinWheel.stopAnimating()
             UIApplication.shared.isNetworkActivityIndicatorVisible = false
             return
         }
@@ -40,6 +45,12 @@ class ViewController: UIViewController, API42Delegate {
         self.navigationController?.pushViewController(userDataVC, animated: true)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        fetchDataLbl.isHidden = true
+        mySpinWheel.stopAnimating()
+        mySpinWheel.hidesWhenStopped = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,6 +62,8 @@ class ViewController: UIViewController, API42Delegate {
     @IBAction func SearchLogin(_ sender: Any) {
         if myConst.token != nil {
             if let userId = myTextField.text, let myAPI42 = API42 {
+                fetchDataLbl.isHidden = false
+                self.mySpinWheel.startAnimating()
                 myAPI42.getUsers(loginStr: userId)
             }
         } else {
